71c52e5df50f59e9346499886caa60fe76b27ff4
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-09 21:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,21 @@
 
 function palindrome(str){
 
+	str = str.split(" ").join("").toLowerCase();
+
+	let i  = 0 ;
+	let j  = str.length-1;
+
+	while(i <= j ){
+
+		if(str[i]!==str[j]){
+			return false;
+		}
+
+		i++;
+		j--;
+	}
+	return true;
+
 }
 module.exports = palindrome
